c27afad67ce2edd63c6f4496b124000b0da2ff25
Steve Galili <steve@meetfeli.com>
2018-10-04 16:32:27 +0200

    Server and client Sockets creation

--- src/nl/dikkeTim/client/Main.java
+++ src/nl/dikkeTim/client/Main.java
@@ -1,8 +1,38 @@
 package nl.dikkeTim.client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        try (Socket socket = new Socket("localhost", 5000)) {
+            BufferedReader echoes = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+            PrintWriter stringToEcho = new PrintWriter(socket.getOutputStream(), true);
 
+            Scanner scanner = new Scanner(System.in);
+            String echoString = "";
+            String response;
+
+            while(!echoString.equals("exit")) {
+                System.out.println("Enter string to be echoed: ");
+                echoString = scanner.nextLine();
+
+                stringToEcho.println(echoString);
+                if(!echoString.equals("exit")) {
+                    response = echoes.readLine();
+                    System.out.println(response);
+                }
+            };
+
+        } catch (IOException e) {
+            System.out.println("Client Error: " + e.getMessage());
+
+        }
     }
 }
