c47b64eb20f0294de8cbf6c83ed8e1955d3744c0
Steve Galili <steve@meetfeli.com>
2018-10-04 16:57:08 +0200

    Echoer creation

--- src/nl/dikkeTim/server/Main.java
+++ src/nl/dikkeTim/server/Main.java
@@ -11,21 +11,11 @@ public class Main {
 
     public static void main(String[] args) {
         try(ServerSocket serverSocket = new ServerSocket(5000)) {
-            Socket socket = serverSocket.accept();
-            System.out.println("Client Connected");
-            BufferedReader input = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream()));
-            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-
             while(true) {
-                String echoString = input.readLine();
-                if(echoString.equals("exit")) {
-                    break;
-                }
-                output.println("Echo from server: " + echoString);
+                Socket socket = serverSocket.accept();
+                Echoer echoer = new Echoer(socket);
+                echoer.start();
             }
-
-
         } catch(IOException e) {
             System.out.println("Server exception " + e.getMessage());
         }
